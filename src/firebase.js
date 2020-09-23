b8ff21786e909c2d075fb424c272afc2c345d355
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-09-23 10:39:10 +1000

    new files

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAuthsw5yghkzBNnXZElECKAsGtVHx2fw",
+  authDomain: "clone-229b8.firebaseapp.com",
+  databaseURL: "https://clone-229b8.firebaseio.com",
+  projectId: "clone-229b8",
+  storageBucket: "clone-229b8.appspot.com",
+  messagingSenderId: "325162080449",
+  appId: "1:325162080449:web:fdd0342880bc63ff3e5c40",
+  measurementId: "G-W1MYJT7EVN",
+};
