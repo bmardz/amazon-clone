7a0e7e16a60cc5877abea9ad7cf5f5f01c4c4e1a
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-12-16 21:39:40 +1100

    updated login

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,15 @@ import './Login.css';
 import { Link } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // firebase code for login
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -14,19 +23,19 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' />
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type='password' />
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className='login__signInButton'>Sign In</button>
+                    <button type='submit' className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
                     By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and Interest-Based Ads Notice.
                 </p>
 
-                <button className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
 
