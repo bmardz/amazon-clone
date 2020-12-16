f42ea4335071739fa4a831940df55ef9209bc0f4
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-12-16 15:46:13 +1100

    updated login, added css

--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,22 @@ function Login() {
 
             <div className="login__container">
                 <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' />
+
+                    <h5>Password</h5>
+                    <input type='password' />
+
+                    <button className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and Interest-Based Ads Notice.
+                </p>
+
+                <button className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
 
