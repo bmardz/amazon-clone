747835950fc007a0a5b6d2185d1bbf8e0677007e
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-12-16 12:27:46 +1100

    added login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,12 +11,17 @@ function App() {
     // BEM
     <Router>
       <div className="App">
-        <Header />
+
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
